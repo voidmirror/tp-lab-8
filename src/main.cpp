cc5f23648f924d0e13669ff76ffdd1691f780d5c
artem <artem.moi@mail.ru>
2020-05-31 01:07:40 +0300

    Lab 8 first commit

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <vector>
+#include <deque>
+#include <cstdlib>
+#include <random>
+#include "task1.h"
+
+using namespace std;
+typedef deque<string> prefix;
+
+int main() {
+	vector<string> text = read_text("text1.txt");
+	string result = generate_text(text);
+	cout << result;
+	ofstream outp("result.txt");
+	outp << result;
+	outp.close();
+	system("pause");
+	return 0;
+}
+
